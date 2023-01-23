115b6163aeca7a269d64566743bc5504d7d62db8
Valerio Ciampoli <ciampolivalerio@gmail.com>
2023-01-23 15:27:01 +0100

    fix list

--- auto/lib/pages/setting_screen.dart
+++ auto/lib/pages/setting_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class SettingScreen extends StatefulWidget {
@@ -12,60 +11,54 @@ bool isActive= true;
 class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+    return Scaffold(
+      body:SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector( 
+              onTap: () {},
+              child:  const Padding( padding: EdgeInsets.all(25.0),
+              child: Text("Informazioni account", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600)),
+            ),), 
+             const Divider(thickness: 2, color: Color.fromARGB(255, 172, 171, 171),),
+            GestureDetector( 
+              onTap: () {},
+              child:  const Padding( padding:  EdgeInsets.all(25.0),
+              child: Text("Cambia lingua", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600)),
+            ),), 
+             const Divider(thickness: 2, color: Color.fromARGB(255, 172, 171, 171),),
+            GestureDetector( 
+            onTap: () {},
+            child: Padding( padding: const EdgeInsets.only(left:25.0, right: 25, top: 15, bottom: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                 const Text("Notifiche", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600)),
+                 Switch.adaptive(value: isActive, onChanged: (value) => setState(() => isActive = value),
+                    activeColor: const Color.fromARGB(255, 76, 172, 251), 
+                    activeTrackColor:const Color.fromARGB(255, 65, 81, 96),
+                    inactiveThumbColor: const Color.fromARGB(255, 183, 181, 181), 
+                    inactiveTrackColor: const Color.fromARGB(255, 107, 106, 106),
+                  )
+              ],),
+          ),), 
+             const Divider(thickness: 2, color: Color.fromARGB(255, 172, 171, 171),),
           GestureDetector( 
             onTap: () {},
-          child: const Padding( padding: EdgeInsets.all(25.0),
-            child: Text("Informazioni personali", style: TextStyle(fontSize: 20)),
+            child: const Padding( padding: EdgeInsets.all(25.0),
+            child: Text("Assistenza", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600)),
           ),), 
-           const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Divider(color: Colors.blue, thickness: 2),
-                  ),
+             const Divider(thickness: 2, color: Color.fromARGB(255, 172, 171, 171),),
           GestureDetector( 
             onTap: () {},
-          child: const Padding( padding: EdgeInsets.all(25.0),
-            child: Text("Cambia lingua", style: TextStyle(fontSize: 20)),
+            child:  const Padding( padding: EdgeInsets.all(25.0),
+            child: Text("Privacy", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600)),
           ),), 
-           const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Divider(color: Colors.blue, thickness: 2),
-                  ),
-                  GestureDetector( 
-          onTap: () {},
-        child: Padding( padding: const EdgeInsets.all(25.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-               const Text("Notifiche", style: TextStyle(fontSize: 20)),
-               Switch.adaptive(value: isActive, onChanged: (value) => setState(() => isActive = value),
-               activeColor: Colors.blue, 
-                activeTrackColor:const Color.fromARGB(255, 179, 177, 177), 
-                inactiveThumbColor: Color.fromARGB(255, 52, 52, 52), 
-                inactiveTrackColor: const Color.fromARGB(255, 179, 177, 177),
-               )
-            ],
-          ),
-        ),), 
-           const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Divider(color: Colors.blue, thickness: 2),
-                  ),
-        GestureDetector( 
-          onTap: () {},
-        child: const Padding( padding: EdgeInsets.all(25.0),
-          child: Text("Assistenza", style: TextStyle(fontSize: 20)),
-        ),), 
-           const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Divider(color: Colors.blue, thickness: 2),
-                  ),
-      ])
+             const Divider(thickness: 2, color: Color.fromARGB(255, 172, 171, 171),),
+        ]),
+      ),
     );
   }
 }
