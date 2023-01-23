93bf8d9cc1aaff238ec363850a9c826517595d13
Valerio Ciampoli <ciampolivalerio@gmail.com>
2023-01-23 11:49:54 +0100

    gfhfgh

--- auto/lib/pages/setting_screen.dart
+++ auto/lib/pages/setting_screen.dart
@@ -1,13 +1,71 @@
 
 import 'package:flutter/material.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
     static const routeName = 'setting-screen.dart';
 
-
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+bool isActive= true;
+class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector( 
+            onTap: () {},
+          child: const Padding( padding: EdgeInsets.all(25.0),
+            child: Text("Informazioni personali", style: TextStyle(fontSize: 20)),
+          ),), 
+           const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Divider(color: Colors.blue, thickness: 2),
+                  ),
+          GestureDetector( 
+            onTap: () {},
+          child: const Padding( padding: EdgeInsets.all(25.0),
+            child: Text("Cambia lingua", style: TextStyle(fontSize: 20)),
+          ),), 
+           const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Divider(color: Colors.blue, thickness: 2),
+                  ),
+                  GestureDetector( 
+          onTap: () {},
+        child: Padding( padding: const EdgeInsets.all(25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+               const Text("Notifiche", style: TextStyle(fontSize: 20)),
+               Switch.adaptive(value: isActive, onChanged: (value) => setState(() => isActive = value),
+               activeColor: Colors.blue, 
+                activeTrackColor:const Color.fromARGB(255, 179, 177, 177), 
+                inactiveThumbColor: Color.fromARGB(255, 52, 52, 52), 
+                inactiveTrackColor: const Color.fromARGB(255, 179, 177, 177),
+               )
+            ],
+          ),
+        ),), 
+           const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Divider(color: Colors.blue, thickness: 2),
+                  ),
+        GestureDetector( 
+          onTap: () {},
+        child: const Padding( padding: EdgeInsets.all(25.0),
+          child: Text("Assistenza", style: TextStyle(fontSize: 20)),
+        ),), 
+           const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Divider(color: Colors.blue, thickness: 2),
+                  ),
+      ])
+    );
   }
 }
